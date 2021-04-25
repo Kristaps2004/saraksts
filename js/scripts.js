983f1e791439b7cff40dd5e57c5d9d992c29cb0b
Kristaps <esmitskristaps@inbox.lv>
2021-04-25 22:33:48 +0300

    izmainas

--- js/scripts.js
+++ js/scripts.js
@@ -24,7 +24,7 @@ document.getElementById('pievienotPreci').addEventListener('click', () => {
 
     render();
 })
-
+    
 function render() {
     let biblioteka = document.getElementById('biblioteka');
     biblioteka.innerHTML = "";
@@ -34,6 +34,7 @@ function render() {
         <div class="prece">
             <h4 class="sadala">Preces nosaukums: ${preces[i].nosaukums}</h4>
             <h4>Daudzums: ${preces[i].daudzums}</h4>
+            <button id="izdzest" onclick="remove(prece)">X</button>
         </div>`;
 
         biblioteka.innerHTML += prece;
@@ -41,3 +42,17 @@ function render() {
 
     localStorage.setItem("preces", JSON.stringify(preces))
 }
+
+var toggled = false,
+    div1 = document.getElementById("izdzest"),
+    div2 = document.getElementById("izdzest"),
+    toggle = function( ) {
+        if( toggled ) {
+           div1.style.display = "block";
+           div2.style.display = "none";
+        } else {
+           div1.style.display = "none";
+           div2.style.display = "block";
+        }
+        toggled = !toggled;
+    };
